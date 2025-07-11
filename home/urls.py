0356ce14de035520c2bb1801590cda289b810842
Ravi Sharma <uvraviz26@gmail.com>
2025-07-11 14:27:20 +0530

    delete

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('mic', views.mic, name='mic'),
-    path('mic_con', views.mic_con, name='mic_con'),
-    path('product', views.product, name='product'),
-]
